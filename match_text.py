ac5ffe8573a3d20bdea3dc29e711119361831c71
pmoriart <moriarty.pam@gmail.com>
2018-08-14 12:34:02 -0700

    try example

--- match_text.py
+++ match_text.py
@@ -34,10 +34,21 @@ descriptions[138]
 diverse_data.iloc[138]
 diverse_data.iloc[0]
 
-nondiv_descriptions = list(non_diverse['description'])
-idx=0
-for description in nondiv_descriptions:
-        nondiv_descriptions[idx] = str(description)
-        idx += 1
-non_div_tfidf = vectorizer.fit_transform(nondiv_descriptions)
-test_similar = linear_kernel(non_div_tfidf[0:1], tfidf).flatten()
+#nondiv_descriptions = list(non_diverse['description'])
+#idx=0
+#for description in nondiv_descriptions:
+#        nondiv_descriptions[idx] = str(description)
+#        idx += 1
+#non_div_tfidf = vectorizer.fit_transform(nondiv_descriptions)
+#test_similar = linear_kernel(non_div_tfidf[0:1], tfidf).flatten()
+
+nondiv_ex = str(non_diverse['description'][0])
+descriptions.insert(0,nondiv_ex)
+#desc_ex = nondiv_ex + descriptions
+tfidf_ex = vectorizer.fit_transform(descriptions)
+cosine_test = linear_kernel(tfidf_ex[0:1], tfidf_ex).flatten()
+related_docs_test = cosine_test.argsort()[:-5:-1]
+cosine_test[related_docs_test]
+related_docs_test
+descriptions[1]
+descriptions[211]
