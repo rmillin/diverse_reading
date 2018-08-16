a8bc25efec81428e13c5c38294b6caad6848328b
Rachel Millin <rmillin@Rachels-MacBook-Pro.local>
2018-08-16 16:01:31 -0700

    updated data0

--- preprocessing/merge_lists.py
+++ preprocessing/merge_lists.py
@@ -7,7 +7,7 @@ Created on Mon Aug 13 11:32:02 2018
 """
 import pandas as pd
 
-fname = '/Users/rmillin/Documents/Insight/diverse-reading/data/diverse_0_01_25.json'
+fname = '/Users/rmillin/Documents/Insight/diverse-reading/data/diverse_0_01_01.json'
 data = pd.read_json(fname)
 data.head()
 
@@ -15,13 +15,18 @@ savename = '/Users/rmillin/Documents/Insight/diverse-reading/data/diverse_books_
 
 diverse_data = pd.read_json(fname)
 
-for num in range(1,13):
+for num in range(1,58):
     try:
-        file_name = '/Users/rmillin/Documents/Insight/diverse-reading/data/diverse_' +str(num) + '_01_25.json'
+        file_name = '/Users/rmillin/Documents/Insight/diverse-reading/data/diverse_' +str(num) + '_01_01.json'
         data_temp = pd.read_json(file_name)
         diverse_data = pd.concat([diverse_data, data_temp], axis=0)
     except:
         print('Skipping ' + file_name)
+
+diverse_data = diverse_data.drop_duplicates(subset=['title','author'])
+diverse_data.to_json(savename, orient='records')
+        
+"""
 for num in range(0,7):
     try:
         file_name = '/Users/rmillin/Documents/Insight/diverse-reading/data/diverse_ya_' +str(num) + '_01_25.json'
@@ -52,3 +57,5 @@ for num in range(0,16):
 
 diverse_data.shape
 non_diverse.shape
+
+"""
--- scrape_goodreads_lists.sh
+++ scrape_goodreads_lists.sh
@@ -4,38 +4,11 @@
 
 cd GoodreadsScraper
 
-diverse_lists=('12716.Speculative_Fiction_by_Authors_of_Color' '116887.2018_Books_by_Authors_of_Color_Native_Authors' '113712.Book_Riot_s_100_Must_Read_Classics_by_People_of_Color' '96119._ReadPOC_List_of_Books_by_Authors_of_Color' '83339.Women_s_Fiction_by_Strong_Women' '22135.Around_the_World_One_Book_from_Each_Country' '4283.Around_the_World_in_100_Books' '5534.Non_American_books_that_every_American_should_read' '71912.Africa_s_100_Best_Books_of_the_20th_Century' '90194.Women_in_Translation' '73176.African_Writers_Series' '95678.Multicultural_Female_Authors' '31853.Iranian_Fiction')
-
-diverse_ya_lists=('104750.Young_Adult_books_with_chronically_ill_physically_or_mentally_disabled_protagonists' '84609.South_Asians_in_Contemporary_YA' '100873.Anticipated_Diverse_2016_2017_YA_Books' '94201.MG_YA_Speculative_Fiction_by_Authors_of_Color' '97984.South_Africa_in_YA_Middle_Grade_Fiction' '92685.International_YA_Books' '104480.Internationally_Minded_YA_Books')
-
-western_classics_lists=('3751.A_Journey_Through_Literary_America' '1126.John_Steinbeck' '9370.Best_of_Hemingway_' '5465.Best_of_Mark_Twain' '21652.Best_of_William_Faulkner' '74999.Historical_Novels_of_Early_America' '36785.WWII_Historic_Fiction' '68.Best_European_Literature' '10785.The_French_Revolution' '3990.Greatest_Eastern_European_Classics' '1339.Best_British_and_Irish_Literature' '1077.Modern_British_Novels' '2457.Best_Books_Of_The_Decade_1880s' '4509.Oh_Canada_' '2458.Best_Books_Of_The_Decade_1860s' '5464.Best_of_Charles_Dickens')
-
-western_ya_lists=('18678.Best_UKYA_Books' '7170.Young_Adult_fiction_by_UK_authors')
+diverse_lists=('11395.Non_Caucasian_Protagonists_in_Science_Fiction_Fantasy_Horror_and_Paranormal_Romance' '203.Native_American_Fiction' '7107.Best_Native_American_Romance' '12763.Best_Native_American_First_Nations_Fiction' '8036.Native_American_Authors' '2508.Favorite_Poets_of_Color' '3739.Teen_Books_With_Native_American_Characters_and_Stories' '338.Immigrant_Experience_Literature' '193.Best_African_American_Books' '27758.Anticipated_Literary_Reads_for_Readers_of_Color_2013' '73078.Diversity_in_Fantasy_and_Science_Fiction' '12800.Best_Black_African_American_Chicklit' '22314.Best_Multicultural_General_Fiction' '3446.African_American_Books_for_Teens' '823.Africa_fiction_and_nonfiction_' '12753.African_Fiction' '48067.Women_Around_the_World' '10961.Fictitious_Africa' '8943.Books_Set_in_Zimbabwe' '12213.Black_Speculative_Fiction' '24317.Plight_of_a_Nation' '71912.Africa_s_100_Best_Books_of_the_20th_Century' '113712.Book_Riot_s_100_Must_Read_Classics_by_People_of_Color' '16436.Arab_Americans_in_Fiction' '338.Immigrant_Experience_Literature' '1336.Best_Middle_East_Fiction' '1337.Best_Middle_East_Nonfiction' '18463.Asian_Fantasy_Science_Fiction' '4323.Iran_and_Iraq_Ancient_and_Modern' '89207.Books_by_LGBTQ_People_of_Color' '76798.Diverse_Horror' '2847.Best_Fiction_With_Disfigured_Disabled_Leads' '8282.Books_with_Disabled_Heros' '10404.Blind_Deaf_Mute' '38493.Characters_with_mental_illnesses_or_learning_disabilites' '756.Best_Books_by_Muslim_Women' '1348.Notable_Books_by_Pakistani_Authors' '18714.1001_Islamic_Books_to_Read_Before_You_Die' '7760.A_Kindle_Muslim_Reading_List' '11481.Islamic_Fiction' '73637.YA_LGBT_Books_Not_Coming_Out_' '91823.Trans_Books_by_Trans_Authors' '653.Best_YA_Fiction_with_GLBTQQI_themes_character' '16026.Diversity_in_Young_Adult_and_Middle_Grade' '3883.Best_Gay_Mystery' '12513.Best_LGBT_Humor' '14155.Latinx_Characters_and_Themes_in_YA' '117159.Latinx_MG_YA_Speculative_Fiction' '118840.2018_books_published_by_for_about_Latinos_Latinas_Latinx' '186.Latino_Latina_Fiction' '2508.Favorite_Poets_of_Color' '14155.Latinx_Characters_and_Themes_in_YA' '23145.Fiction_about_lesbian_bisexual_and_queer_women_of_color' '1341.Best_Asian_American_Teen_Fiction' '42675.Multicultural_New_Adult_Fiction' '46.Best_Feminist_Fiction' '38997.Female_Psychological_Thrillers_Suspense_Written_by_Women' '5401.Strong_Female_Characters_Written_by_Female_Authors' '6934.Science_Fiction_Books_by_Female_Authors')
 
 for((i=0;i<${#diverse_lists[@]};i++))
 do
-    cmd=$(printf './run_scraper.sh %s 1 25 diverse_%s' ${diverse_lists[$i]} "$i")
-    echo $cmd
-    eval $cmd
-done
-
-for((i=0;i<${#diverse_ya_lists[@]};i++))
-do
-    cmd=$(printf './run_scraper.sh %s 1 25 diverse_ya_%s' ${diverse_ya_lists[$i]} "$i")
-    echo $cmd
-    eval $cmd
-done
-
-for((i=0;i<${#western_classics_lists[@]};i++))
-do
-    cmd=$(printf './run_scraper.sh %s 1 25 western_%s' ${western_classics_lists[$i]} "$i")
-    echo $cmd
-    eval $cmd
-done
-
-for((i=0;i<${#western_ya_lists[@]};i++))
-do
-    cmd=$(printf './run_scraper.sh %s 1 25 western_ya_%s' ${western_ya_lists[$i]} "$i")
+    cmd=$(printf './run_scraper.sh %s 1 1 diverse_%s' ${diverse_lists[$i]} "$i")
     echo $cmd
     eval $cmd
 done
