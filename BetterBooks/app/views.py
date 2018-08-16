4787302f63b3c04771f2dc292343a273c7b83261
psilentp <thlindsay1@gmail.com>
2018-08-16 14:02:25 -0700

    validation presents users with two results choices and handles button press

--- BetterBooks/app/views.py
+++ BetterBooks/app/views.py
@@ -23,22 +23,52 @@ def books_output():
 def books_input_val():
     return render_template("input_val.html")
 
+#Button Pressed = 0        
+@app.route('/validation_button_1')
+def button1():
+    print(request.args['t1_choice'].split(','))
+    if request.args['t1_choice'].split(',')[0][1:] == '1':
+        print('user selected on table 1: the recomended results')
+    else:
+        print('user selected on table 1: the random results')
+    return('Thanks!')
+
+#Button Pressed = 0        
+@app.route('/validation_button_2')
+def button2():
+    print(request.args['t2_choice'].split(','))
+    if request.args['t2_choice'].split(',')[0][1:] == '2':
+        print('user selected on table 2: the recomended results')
+    else:
+        print('user selected on table 2: the random results')
+    return('Thanks!')
+
+    #if request.method == "GET":
+    #    return render_template("button.html", ButtonPressed = ButtonPressed)
+    #return redirect(url_for('button'))
 
 @app.route('/output_val')
 def books_output_val():
+    import random
     book_title = request.args.get('book_title')
     book_table = return_match(book_title)
 
-    #random_book_table = pd.DataFrame()
-    #random_book_table['Title'] = ['title1', 'title2', 'title3']
-    #random_book_table['Author'] = ['author1', 'author2', 'author3']
-    #random_book_table['Description'] = ['desc1', 'desc2', 'desc3']
     book_table = return_match(book_title)
-    random_book_table = return_random_match(book_table)
+    random_book_table = return_random_match()
 
-    return render_template("output_val.html", book_recs=book_table,
-                           user_book=book_title, rand_recs=random_book_table)
+    true_table = random.choice([1,2])
+    if true_table == 1:
+        table_1 = book_table
+        table_2 = random_book_table
+    else:
+        table_1 = random_book_table
+        table_2 = book_table
 
+    return render_template("output_val_two_tables.html", 
+                            table_1=table_1,
+                            user_book=book_title,
+                            table_2=table_2,
+                            true_table = true_table)
 
 @app.route('/about')
 def about():
