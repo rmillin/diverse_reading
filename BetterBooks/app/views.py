c5d33182ad8b6373ffb7b8f42d775f757f2f84bd
Benjamin Sperisen <benjamin.sperisen@gmail.com>
2018-08-16 11:29:34 -0700

    duplicated validation page from normal input/output pages, and added link from normal input page.

--- BetterBooks/app/views.py
+++ BetterBooks/app/views.py
@@ -8,7 +8,6 @@ from return_match import return_match
 def books_input():
     return render_template("input.html")
 
-
 @app.route('/output')
 def books_output():
  
@@ -17,7 +16,21 @@ def books_output():
         
     return render_template("output.html", book_recs=book_table, 
                                user_book=book_title)
-        
+
+@app.route('/validation')
+def books_input_val():
+    return render_template("input_val.html")
+
+
+@app.route('/output_val')
+def books_output_val():
+    book_title = request.args.get('book_title')
+    book_table = return_match(book_title)
+
+    return render_template("output_val.html", book_recs=book_table,
+                           user_book=book_title)
+
+
 @app.route('/about')
 def about():
     return render_template("about.html")
